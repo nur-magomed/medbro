3b2b43bc5dfa567130ef1bcdedce890942452615
Nur-Magomed Arsakhanov <nm.arsahanov@voskhod.ru>
2017-10-25 13:06:19 +0300

    Added menu

--- app/src/main/java/com/example/nmarsahanov/medbrat/MainActivity.java
+++ app/src/main/java/com/example/nmarsahanov/medbrat/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nmarsahanov.medbrat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -49,6 +51,25 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse{
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //on menu item click 'about'
+        if (id == R.id.action_about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private String getImageUrl(String med1, String med2) {
